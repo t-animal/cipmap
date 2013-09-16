6bb14723a3cfd41c5d2f50338989cb1e42619230
t.animal <ar79yxiw@cip.cs.fau.de>
2013-09-16 20:57:51 +0200

    Add: Sunrayspy obeys global opt-out

--- server/sunrayspy.sh
+++ server/sunrayspy.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
+filterOptedOut() {
+	while read line; do
+	
+		filter=false
+		username=`echo "$line" | awk '{print $2}'`
+		
+		for ignore in /proj/cipmap/*; do
+		
+			if [[ "/proj/cipmap/$username" == "$ignore" ]]; then
+				filter=true
+			fi
+			
+		done
+		
+		if [[ "$filter" = "false" ]];then
+			echo $line
+		fi
+	done
+}
+
 date +%s > users.`hostname`
-/opt/SUNWut/bin/utwho -c | awk -F'[.]| *' '{ print $5, $6}' >> users.`hostname`
+/opt/SUNWut/bin/utwho -c | awk -F'[.]| *' '{ print $5, $6}' | filterOptedOut  >> users.`hostname`
